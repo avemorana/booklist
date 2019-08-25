f5970b1533f6293fee53d42f1422c44c9978801c
avemorana <ana.shurkina@gmail.com>
2019-08-25 16:00:37 +0300

    add migrations for authors and books tables

---
+++ database/migrations/2019_08_25_123944_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('author_id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('number_of_pages');
+            $table->string('img_path');
+
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
