b7d29def24d9c69fc2459f7d8fb4710fc175f7b5
avemorana <ana.shurkina@gmail.com>
2019-08-25 17:04:03 +0300

    add view files

---
+++ resources/views/book/index.blade.php
@@ -0,0 +1,13 @@
+<table>
+    <th>id</th>
+    <th>title</th>
+    @foreach($books as $book)
+
+        <tr>
+            <td>{{$book->id}}</td>
+            <td>{{$book->title}}</td>
+        </tr>
+
+    @endforeach
+</table>
+{{$books->links()}}
